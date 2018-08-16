4b7c35f1c26b4db7abd5cd5cf39b1b81e3630948
Paul Henschel <drcmda@gmail.com>
2018-08-16 09:52:22 +0200

    2.0, { Grid, Fade, Slug }

--- README.md
+++ README.md
@@ -14,24 +14,28 @@ Demo: https://codesandbox.io/embed/1q178r0rxq
 
 Simplified Demo: https://codesandbox.io/embed/z6ly40071p
 
+## How to use
+
 ```jsx
-import Grid from 'mauerwerk'
+import { Grid } from 'mauerwerk'
 
 <Grid
   // Arbitrary data, should contain keys, possibly heights, etc.
   data={this.state.data}
   // Key accessor, instructs grid on how to fetch individual keys from the data set
   keys={d => d.key}
-  // Can be a fixed value or an individual data accessor (for variable heights)
+  // Can be a fixed value or an individual data accessor (for variable heights), if you leave it undefined it will assume 100% container height
   heights={d => d.height}
   // Optional: number of columns (make it responsive yourself using react-measure/react-media)
-  columns={2}
+  columns={3}
   // Optional: space between elements
-  margin={30}
+  margin={0}
   // Optional: removes the possibility to scroll away from a maximized element
   lockScroll={false}
   // Optional: delay before minimizing an opened element
-  closeDelay={500}>
+  closeDelay={500}
+  // Optional: animates the grid in if true (default)
+  transitionMount={true}>
   {(data, open, toggle) => (
     <div>
       {data.title}
@@ -47,6 +51,41 @@ You feed mauerwerk any data-set (an array of objects most likely), give it acces
 1. `data`, the item from your data-set
 2. `open`, the cells state, depending on whether it is `true` or `false` you can display varying content or use further animation prototypes to transition from cell-state to maximized-state
 3. `toggle`, use this to maximize/minimize your cell
+
+## Effects
+
+You probably don't want flip between open/closed content using static conditions. mauerwerk comes with two simple to use effects:
+
+- `Fade` will fade contents in according to the `show` property. It's a simgple wrapper around react-spring's `Transition`, you can feed it the same properties (config, from, enter, leave, update). By default it will just move opacity from 0 -> 1.
+- `Slug` lets elements "crawl" in. It's a wrapper around react-spring's `Trail`. By default it will move opacity from 0 -> 1 and translate y from 40px -> 0px.
+
+These two would work outside the Grids context if you wanted to use them elsewhere.
+
+```jsx
+import { Grid, Slug, Fade } from 'mauerwerk'
+
+<Grid {...props}>
+  {(data, open, toggle) => (
+    <div>
+      <Fade show={open}>
+        <Slug>
+          <h1>Opened content</h1>
+          <span>Some text</span>
+          <button onClick={toggle}>Close</button>
+        </Slug>
+      </Fade>
+      <Fade show={!open}>
+        <Slug>
+          <h1>Cell content</h1>
+          <span>Some text</span>
+          <button onClick={toggle}>Open</button>
+        </Slug>
+      </Fade>
+      <div>Content that would apply to both open and closed state</div>
+    </div>
+  )}
+</Grid>
+```
 
 ---
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Measure from 'react-measure'
-import { Transition, animated, interpolate } from 'react-spring'
+import { Transition, Trail, animated, interpolate, config } from 'react-spring'
 
 const styles = {
   outer: { position: 'relative', width: '100%', height: '100%' },
   inner: {
     position: 'relative',
     width: '100%',
+    height: '100%',
     overflow: 'hidden',
     minHeight: '100%',
   },
@@ -17,7 +18,7 @@ const styles = {
   },
 }
 
-export default class Grid extends React.Component {
+export class Grid extends React.PureComponent {
   static propTypes = {
     data: PropTypes.array,
     keys: PropTypes.func,
@@ -27,16 +28,16 @@ export default class Grid extends React.Component {
     heights: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
     lockScroll: PropTypes.bool,
     closeDelay: PropTypes.number,
+    transitionMount: PropTypes.bool,
   }
-  static defaultProps = {
-    occupySpace: true,
-    columns: 3,
-    margin: 0,
-    heights: 400,
-    lockScroll: false,
-    closeDelay: 0,
+  state = {
+    width: 0,
+    height: 0,
+    heightOuter: 0,
+    widthOuter: 0,
+    open: undefined,
+    lastOpen: undefined,
   }
-  state = { width: 0, height: 0, open: undefined, lastOpen: undefined }
   scrollOut = e => {
     if (!this.props.lockScroll) {
       this.state.open && this.toggle(undefined)
@@ -73,22 +74,50 @@ export default class Grid extends React.Component {
     this.clicked = false
   }
 
+  cell = ({ key, object }, i) => ({
+    lastOpen,
+    open,
+    opacity,
+    x,
+    y,
+    width,
+    height,
+  }) => (
+    <animated.div
+      style={{
+        ...styles.cell,
+        opacity,
+        width,
+        height,
+        zIndex: lastOpen === key || open === key ? 1000 : i,
+        transform: interpolate(
+          [x, y],
+          (x, y) => `translate3d(${x}px,${y}px, 0)`
+        ),
+      }}
+      children={this.props.children(object, open === key, () =>
+        this.toggle(key)
+      )}
+    />
+  )
+
   render() {
     let {
+      columns = 3,
+      margin = 0,
+      occupySpace = true,
+      lockScroll = false,
+      closeDelay = 0,
+      transitionMount = true,
       children,
-      columns,
-      margin,
-      occupySpace,
       impl,
       config,
       data,
       keys,
       heights,
-      closeDelay,
-      lockScroll,
       ...rest
     } = this.props
-    let { lastOpen, open, width } = this.state
+    let { lastOpen, open, height, width, widthOuter, heightOuter } = this.state
     let column = 0
     let columnHeights = new Array(columns).fill(0)
 
@@ -100,19 +129,25 @@ export default class Grid extends React.Component {
       let left = cellWidth * index
       let offset = (index + 1) * margin
       let top = columnHeights[index] + margin
-      let height = typeof heights === 'function' ? heights(child) : heights
-      columnHeights[index] += height + margin
+      let cellHeight =
+        typeof heights === 'function'
+          ? heights(child)
+          : heights || heightOuter - margin * 2
+      columnHeights[index] += cellHeight + margin
       return {
         x: margin ? left + offset : left,
         y: top,
         width: cellWidth,
-        height,
-        key: keys(child),
+        height: cellHeight,
+        key: keys(child, i),
         object: child,
       }
     })
     const overflow = lockScroll ? (open ? 'hidden' : 'auto') : 'auto'
-    const height = Math.max(...columnHeights) + margin
+    const totalHeight = Math.max(...columnHeights) + margin
+    const renderContainer = widthOuter > 0 && heightOuter > 0
+    const renderContent = transitionMount || (height > 0 && width > 0)
+
     return (
       <Measure
         client
@@ -130,47 +165,123 @@ export default class Grid extends React.Component {
               client
               innerRef={r => (this.innerRef = r)}
               onResize={this.resizeInner}>
-              {({ measureRef }) => (
-                <div ref={measureRef} style={{ ...styles.inner, height }}>
-                  <Transition
-                    native
-                    delay={this.clicked && !open ? closeDelay : 0}
-                    items={displayData}
-                    keys={d => d.key}
-                    from={{ opacity: 0 }}
-                    leave={{ opacity: 0 }}
-                    enter={this.update}
-                    update={this.update}
-                    impl={impl}
-                    config={config}>
-                    {displayData.map(
-                      (c, i) => ({ opacity, x, y, width, height }) => (
-                        <animated.div
-                          style={{
-                            ...styles.cell,
-                            opacity,
-                            width,
-                            height,
-                            zIndex:
-                              lastOpen === c.key || open === c.key ? 1000 : i,
-                            transform: interpolate(
-                              [x, y],
-                              (x, y) => `translate3d(${x}px,${y}px, 0)`
-                            ),
-                          }}
-                          children={children(c.object, open === c.key, () =>
-                            this.toggle(c.key)
-                          )}
-                        />
-                      )
+              {({ measureRef }) =>
+                renderContainer && (
+                  <div
+                    ref={measureRef}
+                    style={{ ...styles.inner, height: totalHeight }}>
+                    {renderContent && (
+                      <Transition
+                        native
+                        delay={this.clicked && !open ? closeDelay : 0}
+                        items={displayData}
+                        keys={d => d.key}
+                        from={{ opacity: 0 }}
+                        leave={{ opacity: 0 }}
+                        enter={this.update}
+                        update={this.update}
+                        impl={impl}
+                        config={config}
+                        lastOpen={lastOpen}
+                        open={open}
+                        children={displayData.map(this.cell)}
+                      />
                     )}
-                  </Transition>
-                </div>
-              )}
+                  </div>
+                )
+              }
             </Measure>
           </div>
         )}
       </Measure>
+    )
+  }
+}
+
+export default props =>
+  console.warn(
+    'The default export will be deprecated next version, import { Grid } from mauerwerk instead!'
+  ) || <Grid {...props} />
+
+const wrap = (child, styles) => {
+  styles = { willChange: Object.keys(styles).join(','), ...styles }
+  if (!animated[child.type]) {
+    // Wrap components into animated divs
+    return <animated.div style={{ ...styles }}>{child}</animated.div>
+  } else {
+    // Otherwise inject styles into existing component-props
+    const Component = animated[child.type]
+    const props = {
+      ...child.props,
+      style: {
+        ...child.props.style,
+        ...styles,
+      },
+    }
+    return <Component {...props} />
+  }
+}
+
+// Wrapper around react-springs Trail component.
+// It will make each child (which must be a dom node) fade and trail in.
+export class Slug extends React.PureComponent {
+  static propTypes = {
+    from: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    to: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  }
+  render() {
+    const {
+      children,
+      from = { opacity: 0, transform: 'translate3d(0,40px,0)' },
+      to = { opacity: 1, transform: 'translate3d(0,0px,0)' },
+      ...rest
+    } = this.props
+    const result = React.Children.map(children, child => styles =>
+      wrap(child, styles)
+    )
+    return (
+      <Trail
+        native
+        {...rest}
+        keys={result.map((_, i) => i)}
+        from={from}
+        to={to}
+        children={result}
+      />
+    )
+  }
+}
+
+// Wrapper around react-springs Transition.
+// It will Transition the child node in and out depending on the "show" prop.
+export class Fade extends React.PureComponent {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    show: PropTypes.bool,
+    from: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    enter: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    leave: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  }
+  render() {
+    const {
+      children,
+      show = true,
+      from = { opacity: 0 },
+      enter = { opacity: 1 },
+      leave = { opacity: 0 },
+      ...rest
+    } = this.props
+    const result = styles => wrap(children, styles)
+    return (
+      <Transition
+        native
+        keys={show.toString()}
+        {...rest}
+        from={from}
+        enter={enter}
+        leave={leave}
+        children={show ? result : undefined}
+      />
     )
   }
 }
